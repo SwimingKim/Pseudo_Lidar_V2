e480794f39f3789eec59f8dc3dd83da5e39b265b
skim <rumex13@naver.com>
2023-08-10 13:07:59 +0900

    regenration pseudo data

--- commands/0_generate_train_txt.sh
+++ commands/0_generate_train_txt.sh
@@ -1,2 +1,11 @@
-cmd=$(ls /Users/suyoung/Documents/share_ubuntu/KITTI/xycar/training/image_2 | sed -e 's/\..*$//' > split/train.txt)
+source=/Users/suyoung/Documents/share_ubuntu/KITTI/xycar/training
+rm -rf $source/depth_map
+rm -rf $source/pseudo_lidar_train
+rm -rf $source/pseudo_lidar_train_planes
+rm -rf $source/pseudo_lidar_train_sparse
+
+ls $source
+
+cmd=$(ls $source/image_2 | sed -e 's/\..*$//' > split/train.txt)
 echo $cmd
+echo $(cat split/train.txt | wc -l)
